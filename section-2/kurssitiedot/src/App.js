419ce6199183940ecd7509b620eadbf15c7784c8
vesteri <arttu.vesterinen@tuni.fi>
2022-04-10 09:59:45 +0300

    exercise 2.5

--- section-2/kurssitiedot/src/App.js
+++ section-2/kurssitiedot/src/App.js
@@ -1,42 +1,5 @@
 import React from 'react';
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  );
-};
-
-const Header = (props) => {
-  return (
-    <div>
-      <h2>{props.text}</h2>
-    </div>
-  );
-};
-
-const Content = ({ parts }) => {
-  let totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
-
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part.name} exercises={part.exercises} />
-      ))}
-      <p style={{ fontWeight: 'bold' }}>Total of {totalExercises} exercises.</p>
-    </div>
-  );
-};
-
-const Course = ({ course }) => (
-  <div>
-    <Header text={course.name} />
-    <Content parts={course.parts} />
-  </div>
-);
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
