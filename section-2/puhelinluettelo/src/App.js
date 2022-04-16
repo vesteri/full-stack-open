9351cbd8dcaac56f200d9845b8b06d18347ff8ab
vesteri <arttu.vesterinen@tuni.fi>
2022-04-16 22:12:00 +0300

    exercise 2.11

--- section-2/puhelinluettelo/src/App.js
+++ section-2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Contacts = ({ persons, filter }) => {
   const filteredPersons = persons.filter((person) =>
@@ -27,15 +28,19 @@ const Input = ({ header, inputString, set }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log('promise fulfilled');
+      setPersons(response.data);
+    });
+  }, []);
+  console.log('render', persons.length, 'notes');
 
   const handleAdd = (event) => {
     event.preventDefault();
